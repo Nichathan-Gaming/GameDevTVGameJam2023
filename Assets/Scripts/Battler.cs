0144c2cbdc27fd2dc6548227927d05c5e305a0c5
Nichathan Gaming <nichathangaming@gmail.com>
2023-05-18 11:54:04 +0700

    Add Scripts folder and Battler class
    The Battler class will be the base for the player and the enemies.

---
+++ Assets/Scripts/Battler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A typical Battler, holds the base values needed for the Player and base Enemy scripts.
+/// </summary>
+public class Battler
+{
+
+}
